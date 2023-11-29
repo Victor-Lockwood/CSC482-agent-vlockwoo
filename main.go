dbd456eb1a517206ba8575b4685d1dbd2ab3573d
Victor-Lockwood <vlockwoo@oswego.edu>
2023-11-29 08:46:07 -0500

    Cleaned up error handling

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 
 func main() {
 	var arg string
-	timeLength := 10000
+	timeLength := 1000
 
 	//Get the length of time for the ticker to run at
 	if len(os.Args) >= 2 {
@@ -28,7 +28,6 @@ func main() {
 	}
 
 	//https://www.tutorialspoint.com/how-to-use-tickers-in-golang
-	//TODO: Figure out how to run it once, since it waits for a tick before running
 	ticker := time.NewTicker(time.Duration(timeLength) * time.Millisecond)
 
 	//Loop through the ticker interval until program stops
@@ -45,16 +44,10 @@ func main() {
 		n2yoKey = os.Getenv("N2YO_KEY")
 		requestUrl = "https://api.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/0/2/&apiKey=" + n2yoKey
 
-		responsePoller, errorPoller := http.Get(requestUrl)
+		responsePoller, _ := http.Get(requestUrl)
 		contentLength := responsePoller.ContentLength
-		//print("Content length: ", contentLength, "\n")
 
-		if errorPoller != nil {
-			err := logglyClient.EchoSend("error", "Got an error while polling.")
-			if err != nil {
-				print("Got error: ", err)
-			}
-		} else if responsePoller.StatusCode == http.StatusOK {
+		if responsePoller.StatusCode == http.StatusOK {
 			defer responsePoller.Body.Close()
 
 			//Used this: https://stackoverflow.com/questions/17156371/how-to-get-json-response-from-http-get
@@ -71,13 +64,7 @@ func main() {
 
 				satelliteInfo := transformApiResponse(satelliteInfoBase)
 
-				//err := logglyClient.EchoSend("info", satelliteInfo.Info.Satname+" Latitude: "+fmt.Sprint(satelliteInfo.Positions[0].Satlatitude)+" Longitude: "+fmt.Sprint(satelliteInfo.Positions[0].Satlongitude))
-
-				err := logglyClient.EchoSend("info", "Content Length: "+fmt.Sprint(contentLength))
-
-				if err != nil {
-					print("Got error while attempting to log response")
-				}
+				logglyClient.EchoSend("info", "Content Length: "+fmt.Sprint(contentLength))
 
 				// Initialize a session that the SDK will use to load
 				// credentials from the shared credentials file ~/.aws/credentials
@@ -90,26 +77,19 @@ func main() {
 				// Create DynamoDB client
 				svc := dynamodb.New(sess)
 
-				av, err := dynamodbattribute.MarshalMap(satelliteInfo)
+				av, _ := dynamodbattribute.MarshalMap(satelliteInfo)
 
 				input := &dynamodb.PutItemInput{
 					Item:      av,
 					TableName: aws.String("vlockwoo-satellites"),
 				}
 
-				_, err = svc.PutItem(input)
-				result, _ := json.Marshal(input)
+				response, _ := svc.PutItem(input)
+				result, _ := json.Marshal(response)
 				fmt.Printf(string(result))
-				if err != nil {
-					logglyClient.EchoSend("error", "Got error calling PutItem")
-					print(err)
-				}
 			}
 		} else {
-			err := logglyClient.EchoSend("error", "Got unexpected response while polling")
-			if err != nil {
-				print("Got error: ", err)
-			}
+			logglyClient.EchoSend("error", "Got unexpected response while polling")
 		}
 	}
 }
